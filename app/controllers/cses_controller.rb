a0162c4e80a84d1963c8bbb6aadeca72930673a7
kninomiya <kounin2017@gmail.com>
2018-12-05 23:33:48 +0900

    “layout”

--- app/controllers/cses_controller.rb
+++ app/controllers/cses_controller.rb
@@ -23,10 +23,11 @@ class CsesController < ApplicationController
     @count = result[:count];
     count = result[:count];
     i = 0
-    flag = []
+    flag = ["OK","OK","NG","NG","OK","NG","NG","NG","OK","OK"];
 
     count.times do |i|
       logger.debug i
+      # open(result[:images][i][:src]) { |image|
       open(result[:images][i][:src]) { |image|
         File.open("test.jpg","wb") do |file|
           file.puts image.read
@@ -36,20 +37,25 @@ class CsesController < ApplicationController
       # Performs label detection on the image file
       labels = vision.image(file_name).labels
       puts "Labels:"
-      flag[i] = "NG"
+      # flag[i] = "NG"
+      puts result[:images][i]
       labels.each do |label|
       puts label.description
       puts label
+
       if label.description == "dog" then
         p "OK"
-        flag[i] = "OK"
+        result[:images][i][:flag] = "OK"
+        # flag[i] = "OK"
       else
+        # result[:images][i] < "NG"
+        result[:images][i][:flag] = "NG"
         p "NG"
       end
       end
     end
     logger.debug result[:images]
-    logger.debug flag
+    # logger.debug 123456--
 
     # 先頭１件の画像URLを出力
     respond_to do |format|
